3fe52f3db289b22e1b0ddaced24a10bc99f8d800
Ryan <ryan.atkinson21@d155.org>
2020-12-07 07:52:35 -0600

    New Branch Ryan A

--- FinalProject_CardGame/ViewController.swift
+++ FinalProject_CardGame/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+         print("Hello")
         // Do any additional setup after loading the view.
     }
-
 
 }
 
